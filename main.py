62c4e59f70a3f2daf1669408bca3ccdfcdc707ab
NgTMDuc <ducntm23052002@gmail.com>
2023-08-22 15:02:57 +0700

    Test config

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from utils.basic_arg import obtain_args
 from data_loader.dataset import HandWritttenDataset
 # print("Hello World!")
 
+def main(args):
+    label_train = args.label_train
+    print(label_train)
 if __name__ == "__main__":
     args = obtain_args()
     for name, value in args._get_kwargs():
         print('{:16} : {:}'.format(name, value))
+    # label_train = args._get_args()
+    main(args)
--- scripts/train.sh
+++ scripts/train.sh
@@ -2,4 +2,4 @@ python BKAI_OCR/main.py \
  --label_train /kaggle/input/handwrittenocr-split/train_list.txt\
  --label_eval  /kaggle/input/handwrittenocr-split/eval_list.txt\
  --train_folder /kaggle/working/new_train \
- --eval_folder /kaggle/working/new_train \       
+ --eval_folder /kaggle/working/new_train
